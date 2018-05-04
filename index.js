377b6385c9e38c45bf4abe32e6b85bb9846aabfd
xxerror500xx <cliff.minks@gmail.com>
2018-05-04 08:25:21 -0500

    Add basic reporting.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var mocha = require('mocha');
 const notifier = require('node-notifier');
+const path = require('path');
+const mochaIcon = path.join(__dirname, 'icons/mocha.svg');
+const errorIcon = path.join(__dirname, '../mocha/images/error.png');
+const okIcon = path.join(__dirname, '../mocha/images/ok.png');
 
 module.exports = MyReporter;
 
 notifier.notify({
-  title: 'Mocha testing START!',
-  message: 'Started!'
+  title: 'Test cycle',
+  message: 'Started',
+  icon: mochaIcon
 });
 function MyReporter(runner) {
   console.log('in mocha-notifier-reporter.js');
@@ -16,26 +21,38 @@ function MyReporter(runner) {
 
   runner.on('pass', function(test){
     passed++;
-    console.log('pass: %s', test.fullTitle());
+    // console.log('pass: %s', test.fullTitle());
   });
 
   runner.on('fail', function(test, err){
     failed++;
-    console.log('fail: %s -- error: %s', test.fullTitle(), err.message);
+    console.log('Fail: %s -- error: %s', test.fullTitle(), err.message);
     notifier.notify({
       title: test.fullTitle(),
-      message: err.message
+      message: err.message,
+      icon: errorIcon
     });
   });
 
   runner.on('end', function(){
+    var iconType = okIcon;
+    if (failed > 0) {
+      iconType = errorIcon;
+    }
+
     total = passed + failed;
     console.log('end: %d/%d', passed, total);
     notifier.notify({
-      title: 'Mocha testing END!',
-      message: 'Pass: ' + passed + ' Failed: ' + failed + ' Total: '+ + total
+      title: 'Test Results',
+      message: ' Failed: ' + failed + ' | Pass: ' + passed + ' | Total: '+ total,
+      icon: iconType
     });
-    process.exit(failed);
+    notifier.notify({
+      title: 'Test cycle',
+      message: 'Ended!',
+      icon: mochaIcon
+    });
+    // process.exit(failed);
   });
 
 }
